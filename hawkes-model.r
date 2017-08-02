04ab609a9ec7ef83cfe1c863a73085737a2be717
Seth Flaxman <flaxman@gmail.com>
2017-08-02 11:20:22 +0100

    use docopt

--- hawkes-model.r
+++ hawkes-model.r
@@ -6,15 +6,29 @@ source("utils.r")
 
 load("WashingtonDC-agls.rdata")
 
-args <- commandArgs(trailingOnly = TRUE)
+library(docopt)
 
-print(args)
-start_date = as.Date(args[1]) 
-end_date = as.Date(args[2]) 
-bw_space = as.numeric(args[3])
-bw_time = as.numeric(args[4])
-separable = as.numeric(args[5]) == 1
-remove.duplicates = as.numeric(args[6]) == 1
+'Usage:
+model-mismatch.r [--start start] [--end end] [--bwSpace bwSpace] [--bwTime bwTime] [--nonseparable] [--duplicates] [--model model]
+
+Options:
+--start start [default: 2010-01-01]
+--end end [default: 2010-01-31]
+--bwSpace bw-space [default: 1.609]
+--nonseparable
+--duplicates
+--bwTime bw-time [default: 14]
+--model model [default: hawkes-model.stan]
+]' -> doc
+
+opts <- docopt(doc)
+
+start_date = as.Date(opts$start)
+end_date = as.Date(opts$end) 
+bw_space = as.numeric(opts$bwSpace)
+bw_time = as.numeric(opts$bwTime)
+separable = !opts$nonseparable
+remove.duplicates = !opts$duplicates
 
 ## Preprocess the data based on the command line arguments
 keep = which(data$date >= start_date & data$date < end_date & data$holiday == F)
@@ -44,6 +58,8 @@ phat.hour = hourly.density[xyt$hour]
 
 phat.space = density.ppp(xy, at="points", sigma=bw_space) / nrow(xyt)
 
+intensity.time = apply(xyt, 1, function(x) sum(epanech2(pdist(matrix(x[3]),matrix(xyt[,3]))@dist, bw_time))/nrow(xyt) )
+
 if(separable) {
 	muhat = intensity.estimate.separable(xyt,bw_space,bw_time)
 } else {
@@ -52,7 +68,7 @@ if(separable) {
 muSTintegral = nrow(xyt)
 
 ## Inference
-m = stan_model("hawkes-model.stan")
+m = stan_model(opts$model)
 data = list(Space=xyt[,1:2],Time=xyt[,3]-min(xyt[,3]), muST=muhat, muSTintegral=muSTintegral, 
 	    n=nrow(xyt),time_window=time.window, space_window=xy.area, hour=round(xyt$T*24) %% 24+1)
 
